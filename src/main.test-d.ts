051a0d41cfd2a3d818e0da7f45350436e2c1b4e4
ehmicky <ehmicky@gmail.com>
2022-06-12 11:42:50 +0200

    Refactoring

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -7,23 +7,23 @@ import {
 } from 'tsd'
 
 expectType<Result>(modernErrors())
+expectAssignable<Result>({ errorHandler: () => new Error('test') })
+expectNotAssignable<Result>({ errorHandler: () => {} })
+
 modernErrors({})
+expectError(modernErrors(true))
 modernErrors({ bugsUrl: '' })
 modernErrors({ bugsUrl: new URL('') })
-modernErrors({ onCreate: (_: Error, __: { anyProp?: boolean }) => {} })
-const { errorHandler } = modernErrors()
-expectType<ErrorHandler>(errorHandler)
-
-expectError(modernErrors(true))
 expectError(modernErrors({ bugsUrl: true }))
+modernErrors({ onCreate: (_: Error, __: { anyProp?: boolean }) => {} })
 expectError(modernErrors({ onCreate: true }))
 expectError(modernErrors({ onCreate: (_: boolean) => {} }))
-
 expectAssignable<Options>({ bugsUrl: '' })
 expectNotAssignable<Options>({ bugsUrl: true })
 
+const { errorHandler } = modernErrors()
+expectType<ErrorHandler>(errorHandler)
+expectType<Error>(errorHandler(undefined))
+expectType<Error>(errorHandler(new Error('test')))
 expectAssignable<ErrorHandler>(() => new Error('test'))
 expectNotAssignable<ErrorHandler>(() => {})
-
-expectAssignable<Result>({ errorHandler: () => new Error('test') })
-expectNotAssignable<Result>({ errorHandler: () => {} })
