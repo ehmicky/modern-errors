39d98eddfa48ad8a19a5f663e72635bd0aaf9541
ehmicky <ehmicky@gmail.com>
2022-06-12 11:57:33 +0200

    Improve types

--- src/main.d.ts
+++ src/main.d.ts
@@ -8,37 +8,15 @@ import { ErrorName, OnCreate, ErrorType } from 'error-type'
  */
 export default function modernErrors(options?: Options): Result
 
-/**
- *
- */
 export interface Options {
   bugsUrl?: string | URL
   onCreate?: OnCreate
 }
 
-/**
- *
- */
-export type Result = { errorHandler: ErrorHandler } & {
-  [errorName in ErrorName]: ErrorType
+export type Result = { [errorName in ErrorName]: ErrorType } & {
+  errorHandler: ErrorHandler
 }
 
-/**
- *
- */
-export type ErrorHandler = (error: any) => Error
+export type ErrorHandler = (error: unknown) => Error
 
-/**
- *
- */
-export type { ErrorName }
-
-/**
- *
- */
-export type { OnCreate }
-
-/**
- *
- */
-export type { ErrorType }
+export type { ErrorName, OnCreate, ErrorType }
