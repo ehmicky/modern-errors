9032de623a87aed1c502d1bf723eb10da41cbec3
ehmicky <ehmicky@gmail.com>
2022-08-14 17:34:11 +0200

    Add more tests

--- test/serialize.js
+++ test/serialize.js
@@ -12,3 +12,20 @@ test('Can serialize with error.toJSON()', (t) => {
     cause: { name: 'Error', message: cause.message, stack: cause.stack },
   })
 })
+
+test('Can use parse()', (t) => {
+  const { InputError, parse } = modernErrors()
+  const cause = new Error('innerTest')
+  const error = new InputError('test', { cause })
+  t.deepEqual(parse(error.toJSON()), error)
+})
+
+test('Can use serialize and parse deeply', (t) => {
+  const { InputError, parse } = modernErrors()
+  const cause = new Error('innerTest')
+  const error = new InputError('test', { cause })
+  const jsonString = JSON.stringify([{ error, prop: true }])
+  const parsedValue = JSON.parse(jsonString, (key, value) => parse(value))
+  t.true(parsedValue[0].prop)
+  t.deepEqual(parsedValue[0].error, error)
+})
