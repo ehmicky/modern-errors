14a5c9655f94e918757157c87a47c967d94db7f6
ehmicky <ehmicky@gmail.com>
2022-09-18 11:37:21 +0200

    Improve tests

--- test/create/name.js
+++ test/create/name.js
@@ -1,19 +1,21 @@
 import test from 'ava'
-import modernErrors from 'modern-errors'
 import { each } from 'test-each'
 
-each([undefined, '', {}], ({ title }, errorName) => {
-  test(`Validate invalid error name | ${title}`, (t) => {
-    t.throws(modernErrors().create.bind(undefined, errorName))
-  })
-})
+import { createAnyError, defineSimpleClass } from '../helpers/main.js'
+
+const { AnyError } = defineSimpleClass()
 
 test('Require defining UnknownError', (t) => {
-  t.throws(modernErrors().create.bind(undefined, 'InputError'))
+  const { create } = createAnyError()
+  t.throws(create.bind(undefined, 'InputError'))
 })
 
 test('Validate against duplicate names', (t) => {
-  const { create } = modernErrors()
-  create('UnknownError')
-  t.throws(create.bind(undefined, 'UnknownError'))
+  t.throws(AnyError.create.bind(undefined, 'TestError'))
+})
+
+each([undefined, '', {}, 'AnyError'], ({ title }, errorName) => {
+  test(`Validate invalid error name | ${title}`, (t) => {
+    t.throws(AnyError.create.bind(undefined, errorName))
+  })
 })
