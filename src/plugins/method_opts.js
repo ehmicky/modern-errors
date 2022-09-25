040cc68fea732682ed685b060ba6f05221c9c00c
ehmicky <ehmicky@gmail.com>
2022-09-25 23:50:37 +0200

    Add comment

--- src/plugins/method_opts.js
+++ src/plugins/method_opts.js
@@ -6,6 +6,17 @@ export const defaultIsOptions = function () {
   return true
 }
 
+// Options can be passed as the last argument of `static|instanceMethods`.
+// `plugin.isOptions(lastArgument) => boolean` can be defined to distinguish
+// that last options argument from other arguments for any of those methods.
+// `plugin.isOptions()` should be as wide as possible:
+//   - It should only distinguish `option` from methods actual last arguments
+//   - While still ensuring invalid options are passed to `plugin.normalize()`
+//     instead of considering them not options
+// `plugin.isOptions()` has the following props:
+//  - Users can pass `options[pluginName]` instead of `options`
+//  - Plugin methods can have variadic and optional parameters
+//  - It does not rely on brittle `Function.length`
 export const applyIsOptions = function ({
   args,
   pluginsOpts,
