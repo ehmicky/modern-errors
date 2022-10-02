d83c54d7788506201ccf73c18cb2cd65f7ae0ada
ehmicky <ehmicky@gmail.com>
2022-10-02 19:26:14 +0200

    Refactoring

--- test/core_plugins/winston/static.js
+++ test/core_plugins/winston/static.js
@@ -1,57 +1,25 @@
 import test from 'ava'
 import { serialize } from 'error-serializer'
 import { each } from 'test-each'
-import through from 'through2'
-import { MESSAGE } from 'triple-beam'
-import { createLogger, transports, format } from 'winston'
 
-import { TestError, AnyError } from '../../helpers/winston.js'
-
-// eslint-disable-next-line fp/no-let
-let shortLog = ''
-const shortStream = through.obj((object, encoding, done) => {
-  // eslint-disable-next-line fp/no-mutation
-  shortLog = object[MESSAGE]
-  done(undefined, object)
-})
-const shortLogger = createLogger({
-  format: format.combine(AnyError.shortFormat(), format.simple()),
-  transports: [new transports.Stream({ stream: shortStream })],
-})
-
-const doShortLog = function (value) {
-  shortLogger.error(value)
-  return shortLog
-}
-
-// eslint-disable-next-line fp/no-let
-let fullLog = {}
-const fullStream = through.obj((object, encoding, done) => {
-  // eslint-disable-next-line fp/no-mutation
-  fullLog = JSON.parse(object[MESSAGE])
-  done(undefined, object)
-})
-const fullLogger = createLogger({
-  format: format.combine(AnyError.fullFormat(), format.json()),
-  transports: [new transports.Stream({ stream: fullStream })],
-})
-
-const doFullLog = function (value) {
-  fullLogger.error(value)
-  return fullLog
-}
+import {
+  TestError,
+  AnyError,
+  shortLog,
+  fullLog,
+} from '../../helpers/winston.js'
 
 const testLevel = 'warn'
 const defaultLevel = 'error'
 
 test.serial('Log known errors with shortFormat', (t) => {
   const error = new TestError('test', { winston: { level: testLevel } })
-  t.is(doShortLog(error), `${testLevel}: ${error.name}: ${error.message}`)
+  t.is(shortLog(error), `${testLevel}: ${error.name}: ${error.message}`)
 })
 
 test.serial('Log known errors with fullFormat', (t) => {
   const error = new TestError('test', { winston: { level: testLevel } })
-  t.deepEqual(doFullLog(error), {
+  t.deepEqual(fullLog(error), {
     level: testLevel,
     name: error.name,
     message: error.message,
@@ -61,26 +29,26 @@ test.serial('Log known errors with fullFormat', (t) => {
 test.serial('Log unknown errors with shortFormat', (t) => {
   const error = new Error('test')
   const { stack } = AnyError.normalize(error)
-  t.is(doShortLog(error), `${defaultLevel}: ${stack}`)
+  t.is(shortLog(error), `${defaultLevel}: ${stack}`)
 })
 
 test.serial('Log unknown errors with fullFormat', (t) => {
   const error = new Error('test')
   const { name, message, stack } = AnyError.normalize(error)
-  t.deepEqual(doFullLog(error), { level: defaultLevel, name, message, stack })
+  t.deepEqual(fullLog(error), { level: defaultLevel, name, message, stack })
 })
 
 test.serial('Log non-errors with shortFormat', (t) => {
   const message = 'test'
-  t.is(doShortLog(message), `${defaultLevel}: ${message}`)
+  t.is(shortLog(message), `${defaultLevel}: ${message}`)
 })
 
 test.serial('Log non-errors with fullFormat', (t) => {
   const message = 'test'
-  t.deepEqual(doFullLog(message), { level: defaultLevel, message })
+  t.deepEqual(fullLog(message), { level: defaultLevel, message })
 })
 
-each([doShortLog, doFullLog], ({ title }, doLog) => {
+each([shortLog, fullLog], ({ title }, doLog) => {
   test.serial(`Does not modify error | ${title}`, (t) => {
     const error = new TestError('test', { winston: { level: testLevel } })
     const keysBefore = Reflect.ownKeys(error)
--- test/helpers/winston.js
+++ test/helpers/winston.js
@@ -1,3 +1,7 @@
+import through from 'through2'
+import { MESSAGE } from 'triple-beam'
+import { createLogger, transports, format } from 'winston'
+
 // eslint-disable-next-line no-restricted-imports
 import WINSTON_PLUGIN from '../../src/core_plugins/winston/main.js'
 
@@ -10,3 +14,37 @@ export const unknownError = new AnyError('test', { cause: '' })
 export const warnError = new TestError('test', { winston: { level: 'warn' } })
 export const noStackError = new TestError('test', { winston: { stack: false } })
 export const yesStackError = new TestError('test', { winston: { stack: true } })
+
+// eslint-disable-next-line fp/no-let
+let lastShortLog = ''
+const shortStream = through.obj((object, encoding, done) => {
+  // eslint-disable-next-line fp/no-mutation
+  lastShortLog = object[MESSAGE]
+  done(undefined, object)
+})
+const shortLogger = createLogger({
+  format: format.combine(AnyError.shortFormat(), format.simple()),
+  transports: [new transports.Stream({ stream: shortStream })],
+})
+
+export const shortLog = function (value) {
+  shortLogger.error(value)
+  return lastShortLog
+}
+
+// eslint-disable-next-line fp/no-let
+let lastFullLog = {}
+const fullStream = through.obj((object, encoding, done) => {
+  // eslint-disable-next-line fp/no-mutation
+  lastFullLog = JSON.parse(object[MESSAGE])
+  done(undefined, object)
+})
+const fullLogger = createLogger({
+  format: format.combine(AnyError.fullFormat(), format.json()),
+  transports: [new transports.Stream({ stream: fullStream })],
+})
+
+export const fullLog = function (value) {
+  fullLogger.error(value)
+  return lastFullLog
+}
