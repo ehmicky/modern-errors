df1c3f9fff2346fdb1cf227720ae8454515ea446
ehmicky <ehmicky@gmail.com>
2022-10-02 19:26:01 +0200

    Refactoring

--- test/core_plugins/winston/static.js
+++ test/core_plugins/winston/static.js
@@ -19,6 +19,11 @@ const shortLogger = createLogger({
   transports: [new transports.Stream({ stream: shortStream })],
 })
 
+const doShortLog = function (value) {
+  shortLogger.error(value)
+  return shortLog
+}
+
 // eslint-disable-next-line fp/no-let
 let fullLog = {}
 const fullStream = through.obj((object, encoding, done) => {
@@ -31,19 +36,22 @@ const fullLogger = createLogger({
   transports: [new transports.Stream({ stream: fullStream })],
 })
 
+const doFullLog = function (value) {
+  fullLogger.error(value)
+  return fullLog
+}
+
 const testLevel = 'warn'
 const defaultLevel = 'error'
 
 test.serial('Log known errors with shortFormat', (t) => {
   const error = new TestError('test', { winston: { level: testLevel } })
-  shortLogger.error(error)
-  t.is(shortLog, `${testLevel}: ${error.name}: ${error.message}`)
+  t.is(doShortLog(error), `${testLevel}: ${error.name}: ${error.message}`)
 })
 
 test.serial('Log known errors with fullFormat', (t) => {
   const error = new TestError('test', { winston: { level: testLevel } })
-  fullLogger.error(error)
-  t.deepEqual(fullLog, {
+  t.deepEqual(doFullLog(error), {
     level: testLevel,
     name: error.name,
     message: error.message,
@@ -53,36 +61,32 @@ test.serial('Log known errors with fullFormat', (t) => {
 test.serial('Log unknown errors with shortFormat', (t) => {
   const error = new Error('test')
   const { stack } = AnyError.normalize(error)
-  shortLogger.error(error)
-  t.is(shortLog, `${defaultLevel}: ${stack}`)
+  t.is(doShortLog(error), `${defaultLevel}: ${stack}`)
 })
 
 test.serial('Log unknown errors with fullFormat', (t) => {
   const error = new Error('test')
   const { name, message, stack } = AnyError.normalize(error)
-  fullLogger.error(error)
-  t.deepEqual(fullLog, { level: defaultLevel, name, message, stack })
+  t.deepEqual(doFullLog(error), { level: defaultLevel, name, message, stack })
 })
 
 test.serial('Log non-errors with shortFormat', (t) => {
   const message = 'test'
-  shortLogger.error(message)
-  t.is(shortLog, `${defaultLevel}: ${message}`)
+  t.is(doShortLog(message), `${defaultLevel}: ${message}`)
 })
 
 test.serial('Log non-errors with fullFormat', (t) => {
   const message = 'test'
-  fullLogger.error(message)
-  t.deepEqual(fullLog, { level: defaultLevel, message })
+  t.deepEqual(doFullLog(message), { level: defaultLevel, message })
 })
 
-each([shortLogger, fullLogger], ({ title }, logger) => {
+each([doShortLog, doFullLog], ({ title }, doLog) => {
   test.serial(`Does not modify error | ${title}`, (t) => {
     const error = new TestError('test', { winston: { level: testLevel } })
     const keysBefore = Reflect.ownKeys(error)
     const valuesBefore = serialize(error)
 
-    logger.error(error)
+    doLog(error)
 
     const keysAfter = Reflect.ownKeys(error)
     const valuesAfter = serialize(error)
