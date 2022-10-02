63087675aaf506af5c3d61dae9aaaf8ecd397f95
ehmicky <ehmicky@gmail.com>
2022-10-02 11:26:23 +0200

    Add modern-errors-cli

---
+++ src/core_plugins/cli.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line filenames/match-exported
+import handleCliError from 'handle-cli-error'
+
+const getOptions = function (options = {}) {
+  return options
+}
+
+const exit = function ({ options }) {
+  handleCliError(options)
+}
+
+const CLI_PLUGIN = { name: 'cli', getOptions, instanceMethods: { exit } }
+
+// eslint-disable-next-line import/no-default-export
+export default CLI_PLUGIN
