3f4e2558ccc0193deeae18d4cdedaac43189efbd
ehmicky <ehmicky@gmail.com>
2022-10-02 19:26:24 +0200

    Refactoring

--- test/helpers/winston.js
+++ test/helpers/winston.js
@@ -15,36 +15,46 @@ export const warnError = new TestError('test', { winston: { level: 'warn' } })
 export const noStackError = new TestError('test', { winston: { stack: false } })
 export const yesStackError = new TestError('test', { winston: { stack: true } })
 
-// eslint-disable-next-line fp/no-let
-let lastShortLog = ''
-const shortStream = through.obj((object, encoding, done) => {
-  // eslint-disable-next-line fp/no-mutation
-  lastShortLog = object[MESSAGE]
-  done(undefined, object)
-})
-const shortLogger = createLogger({
-  format: format.combine(AnyError.shortFormat(), format.simple()),
-  transports: [new transports.Stream({ stream: shortStream })],
-})
-
 export const shortLog = function (value) {
   shortLogger.error(value)
   return lastShortLog
 }
 
 // eslint-disable-next-line fp/no-let
-let lastFullLog = {}
-const fullStream = through.obj((object, encoding, done) => {
-  // eslint-disable-next-line fp/no-mutation
-  lastFullLog = JSON.parse(object[MESSAGE])
-  done(undefined, object)
-})
-const fullLogger = createLogger({
-  format: format.combine(AnyError.fullFormat(), format.json()),
-  transports: [new transports.Stream({ stream: fullStream })],
-})
+let lastShortLog = ''
+
+const getShortLogger = function () {
+  const shortStream = through.obj((object, encoding, done) => {
+    // eslint-disable-next-line fp/no-mutation
+    lastShortLog = object[MESSAGE]
+    done(undefined, object)
+  })
+  return createLogger({
+    format: format.combine(AnyError.shortFormat(), format.simple()),
+    transports: [new transports.Stream({ stream: shortStream })],
+  })
+}
+
+const shortLogger = getShortLogger()
 
 export const fullLog = function (value) {
   fullLogger.error(value)
   return lastFullLog
 }
+
+// eslint-disable-next-line fp/no-let
+let lastFullLog = {}
+
+const getFullLogger = function () {
+  const fullStream = through.obj((object, encoding, done) => {
+    // eslint-disable-next-line fp/no-mutation
+    lastFullLog = JSON.parse(object[MESSAGE])
+    done(undefined, object)
+  })
+  return createLogger({
+    format: format.combine(AnyError.fullFormat(), format.json()),
+    transports: [new transports.Stream({ stream: fullStream })],
+  })
+}
+
+const fullLogger = getFullLogger()
