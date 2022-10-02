08c62052d01070e994b9f65c043d7dba9674b490
ehmicky <ehmicky@gmail.com>
2022-10-02 11:54:53 +0200

    Add more tests

--- test/plugins_list/cli.js
+++ test/plugins_list/cli.js
@@ -4,12 +4,15 @@ import { each } from 'test-each'
 // eslint-disable-next-line no-restricted-imports
 import CLI_PLUGIN from '../../src/core_plugins/cli.js'
 import { errorExit } from '../helpers/cli/main.js'
-import { defineClassOpts } from '../helpers/main.js'
+import { defineClassOpts, defineClassesOpts } from '../helpers/main.js'
 
-const { TestError } = defineClassOpts({}, {}, [CLI_PLUGIN])
+const testGlobalOpts = { cli: { timeout: 0 } }
+const { TestError, AnyError } = defineClassOpts({}, testGlobalOpts, [
+  CLI_PLUGIN,
+])
 
 const message = 'test'
-const testError = new TestError(message, { cli: { timeout: 0 } })
+const testError = new TestError(message)
 
 each(
   [true, { timeout: 'true' }, { unknown: true }, { classes: {} }],
@@ -21,7 +24,44 @@ each(
 )
 
 test.serial('Call process.exit()', (t) => {
-  const { consoleArg, exitCode } = errorExit(testError)
-  t.true(Number.isInteger(exitCode))
-  t.is(consoleArg, message)
+  t.true(Number.isInteger(errorExit(testError).exitCode))
+})
+
+test.serial('Can pass "exitCode"', (t) => {
+  const exitCode = 5
+  t.is(errorExit(testError, { exitCode }).exitCode, exitCode)
+})
+
+test.serial('"exitCode" defaults to incrementing number', (t) => {
+  const exitCode = 5
+  const { UnknownError, OneError, TwoError, ThreeError } = defineClassesOpts(
+    {
+      OneError: {},
+      TwoError: { cli: { exitCode } },
+      ThreeError: {},
+    },
+    testGlobalOpts,
+    [CLI_PLUGIN],
+  )
+  t.is(errorExit(new UnknownError('')).exitCode, 1)
+  t.is(errorExit(new OneError('')).exitCode, 2)
+  t.is(errorExit(new TwoError('')).exitCode, exitCode)
+  // eslint-disable-next-line no-magic-numbers
+  t.is(errorExit(new ThreeError('')).exitCode, 4)
+})
+
+test.serial('Can pass "silent"', (t) => {
+  t.is(errorExit(testError, { silent: true }).consoleArg, undefined)
+})
+
+test.serial('Can pass "short"', (t) => {
+  t.is(errorExit(testError, { short: false }).consoleArg, testError)
+})
+
+test.serial('"short" defaults to true', (t) => {
+  t.is(errorExit(testError).consoleArg, message)
+})
+
+test.serial('"short" is false with unknown errors', (t) => {
+  t.true(errorExit(new AnyError('', { cause: '' })).consoleArg instanceof Error)
 })
