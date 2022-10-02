06110b29dfbebe03c5dabf07290ee4cd90b1c0af
ehmicky <ehmicky@gmail.com>
2022-10-02 23:32:05 +0200

    Add a test

--- test/plugins/previous.js
+++ test/plugins/previous.js
@@ -76,3 +76,17 @@ test('plugin.set() properties reverts cannot be mutated', (t) => {
   const error = new TestError('test', { cause })
   t.true(error.one.two)
 })
+
+test('plugin.set() values that have not changed are not reverted', (t) => {
+  const deepCause = new TestError('deepCauseMessage')
+  // eslint-disable-next-line fp/no-mutation
+  deepCause.one = true
+  const cause = new TestError('causeMessage', {
+    cause: deepCause,
+    prop: { toSet: { one: true } },
+  })
+  // eslint-disable-next-line fp/no-mutation
+  cause.one = false
+  const error = new TestError('test', { cause })
+  t.false(error.one)
+})
