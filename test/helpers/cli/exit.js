a204650f3614f6be3010f42a9b925f7c5581287f
ehmicky <ehmicky@gmail.com>
2022-10-02 11:53:31 +0200

    Add CLI utilities

---
+++ test/helpers/cli/exit.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line fp/no-let, init-declarations
+let originalProcessExit
+// eslint-disable-next-line fp/no-let, init-declarations
+let exitFuncCode
+
+// Mock `process.exit()` during tests
+export const mockProcessExit = function () {
+  // eslint-disable-next-line n/prefer-global/process
+  const { process } = globalThis
+
+  if (process === undefined) {
+    return
+  }
+
+  // eslint-disable-next-line fp/no-mutation
+  originalProcessExit = process.exit
+  // eslint-disable-next-line fp/no-mutation
+  process.exit = mockedProcessExit
+}
+
+const mockedProcessExit = function (exitCode) {
+  // eslint-disable-next-line fp/no-mutation
+  exitFuncCode = exitCode
+}
+
+// Retrieve value passed to `process.exitCode` and mocked `process.exit()`
+export const getProcessExitCode = function () {
+  return exitFuncCode
+}
+
+// Reverse `mockProcessExit()`
+export const unmockProcessExit = function () {
+  // eslint-disable-next-line n/prefer-global/process
+  const { process } = globalThis
+
+  if (process === undefined) {
+    return
+  }
+
+  // eslint-disable-next-line fp/no-mutation
+  process.exit = originalProcessExit
+  // eslint-disable-next-line fp/no-mutation
+  exitFuncCode = undefined
+}
---
+++ test/helpers/cli/main.js
@@ -0,0 +1,23 @@
+import { mockConsole, unmockConsole, getConsoleArg } from './console.js'
+import {
+  mockProcessExit,
+  unmockProcessExit,
+  getProcessExitCode,
+} from './exit.js'
+
+// `modern-errors-cli` use global variables `process.exitCode`, `process.exit()`
+// and `console.error()` so we need to mock them.
+export const handleError = function (error, options) {
+  mockConsole()
+  mockProcessExit()
+
+  try {
+    error.exit(options)
+    const consoleArg = getConsoleArg()
+    const exitCode = getProcessExitCode()
+    return { consoleArg, exitCode }
+  } finally {
+    unmockProcessExit()
+    unmockConsole()
+  }
+}
