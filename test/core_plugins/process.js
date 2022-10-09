9055d26989f9f469ab0f4282170d23a360d732e6
ehmicky <ehmicky@gmail.com>
2022-10-09 13:15:10 +0200

    Add some tests

--- test/core_plugins/process.js
+++ test/core_plugins/process.js
@@ -12,7 +12,9 @@ import { defineClassOpts } from '../helpers/main.js'
 // TODO: use `timers/promises` after dropping support for Node <15
 const pSetInterval = promisify(setInterval)
 
-const { UnknownError, AnyError } = defineClassOpts({}, {}, [PROCESS_PLUGIN])
+const { TestError, UnknownError, AnyError } = defineClassOpts({}, {}, [
+  PROCESS_PLUGIN,
+])
 
 each(
   [true, { unknown: true }, { exit: 'true' }, { onError: true }],
@@ -24,6 +26,19 @@ each(
   },
 )
 
+// eslint-disable-next-line no-restricted-globals
+const consoleError = sinon.stub(console, 'error')
+
+test.serial('Prints on the console by default', async (t) => {
+  const stopLogging = AnyError.logProcess()
+  const error = new UnknownError('test')
+  emitWarning(error)
+  await pSetInterval()
+  t.is(consoleError.args[0][0], error)
+  stopLogging()
+  consoleError.restore()
+})
+
 test.serial('Handles process errors', async (t) => {
   const onError = sinon.spy()
   const stopLogging = AnyError.logProcess({ onError })
@@ -34,15 +49,31 @@ test.serial('Handles process errors', async (t) => {
   stopLogging()
 })
 
-// eslint-disable-next-line no-restricted-globals
-const stub = sinon.stub(console, 'error')
-
-test.serial('Prints on the console by default', async (t) => {
-  const stopLogging = AnyError.logProcess()
-  const error = new UnknownError('test')
-  emitWarning(error)
-  await pSetInterval()
-  t.is(stub.args[0][0], error)
-  stopLogging()
-  stub.restore()
-})
+each(
+  [
+    { error: 'test', message: 'Warning: test' },
+    { error: new Error('test'), message: 'test' },
+    { error: new TypeError('test'), message: 'test' },
+    {
+      // eslint-disable-next-line fp/no-mutating-assign
+      error: Object.assign(new Error('test'), { name: 'NamedError' }),
+      message: 'NamedError: test',
+    },
+    { error: new UnknownError('test'), message: 'test' },
+    { error: new TestError('test'), message: 'TestError: test' },
+  ],
+  ({ title }, { error, message }) => {
+    test.serial(
+      `Process errors are normalized to UnknownError | ${title}`,
+      async (t) => {
+        const onError = sinon.spy()
+        const stopLogging = AnyError.logProcess({ onError })
+        emitWarning(error)
+        await pSetInterval()
+        t.true(onError.args[0][0] instanceof UnknownError)
+        t.is(onError.args[0][0].message, message)
+        stopLogging()
+      },
+    )
+  },
+)
