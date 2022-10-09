cf0366d57575fe1d4d9da63e74f6f74c27db1c89
ehmicky <ehmicky@gmail.com>
2022-10-09 13:58:42 +0200

    Refactoring

--- test/helpers/info.js
+++ test/helpers/info.js
@@ -1,24 +1,20 @@
 // Retrieve `info` passed to `plugin.properties()`
-export const getPropertiesInfo = function ({
-  ErrorClasses: { TestError: TestErrorClass },
-  instanceOpts,
-}) {
-  return new TestErrorClass('test', instanceOpts).properties
+export const getPropertiesInfo = function ({ ErrorClasses, instanceOpts }) {
+  return new ErrorClasses.TestError('test', instanceOpts).properties
 }
 
 // Retrieve `info` passed to `plugin.instanceMethods`
 export const getInstanceInfo = function ({
-  ErrorClasses: { TestError: TestErrorClass },
+  ErrorClasses,
   instanceOpts,
   methodOpts,
 }) {
-  return new TestErrorClass('test', instanceOpts).getInstance(methodOpts)
+  return new ErrorClasses.TestError('test', instanceOpts).getInstance(
+    methodOpts,
+  )
 }
 
 // Retrieve `info` passed to `plugin.staticMethods`
-export const getStaticInfo = function ({
-  ErrorClasses: { AnyError: AnyErrorClass },
-  methodOpts,
-}) {
-  return AnyErrorClass.getProp(methodOpts)
+export const getStaticInfo = function ({ ErrorClasses, methodOpts }) {
+  return ErrorClasses.AnyError.getProp(methodOpts)
 }
