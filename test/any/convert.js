306213d80ddff7cea8adbf1c8468c9ee1ef0b061
ehmicky <ehmicky@gmail.com>
2022-10-09 13:41:28 +0200

    Split file

--- test/any/convert.js
+++ test/any/convert.js
@@ -8,40 +8,8 @@ import {
   KnownErrorClasses,
   UnknownErrorClasses,
   getKnownErrors,
-  getUnknownErrors,
   getUnknownErrorInstances,
 } from '../helpers/known.js'
-
-const assertInstanceOf = function (t, error, ErrorClass) {
-  t.true(error instanceof ErrorClass)
-  t.is(Object.getPrototypeOf(error), ErrorClass.prototype)
-  t.is(error.name, ErrorClass.name)
-}
-
-each(getKnownErrors(), ({ title }, cause) => {
-  test(`AnyError with known cause uses child class | ${title}`, (t) => {
-    const error = new AnyError('message', { cause })
-    assertInstanceOf(t, error, cause.constructor)
-  })
-})
-
-each(getUnknownErrors(), ({ title }, cause) => {
-  test(`AnyError with unknown cause uses UnknownError | ${title}`, (t) => {
-    const error = new AnyError('message', { cause })
-    assertInstanceOf(t, error, UnknownError)
-  })
-})
-
-each(
-  [...KnownErrorClasses, ...UnknownErrorClasses],
-  [...getKnownErrors(), ...getUnknownErrors()],
-  ({ title }, ErrorClass, cause) => {
-    test(`Known class with known or unknown cause uses parent class | ${title}`, (t) => {
-      const error = new ErrorClass('message', { cause })
-      assertInstanceOf(t, error, ErrorClass)
-    })
-  },
-)
 
 each(
   [AnyError, ...KnownErrorClasses, ...UnknownErrorClasses],
--- test/any/merge.js
+++ test/any/merge.js
@@ -1,9 +1,46 @@
 import test from 'ava'
 import { each } from 'test-each'
 
-import { defineClassOpts } from '../helpers/main.js'
+import {
+  TestError,
+  UnknownError,
+  AnyError,
+  KnownErrorClasses,
+  UnknownErrorClasses,
+  getKnownErrors,
+  getUnknownErrors,
+} from '../helpers/known.js'
 
-const { TestError, AnyError } = defineClassOpts()
+const assertInstanceOf = function (t, error, ErrorClass) {
+  t.true(error instanceof ErrorClass)
+  t.is(Object.getPrototypeOf(error), ErrorClass.prototype)
+  t.is(error.name, ErrorClass.name)
+}
+
+each(getKnownErrors(), ({ title }, cause) => {
+  test(`AnyError with known cause uses child class | ${title}`, (t) => {
+    const error = new AnyError('message', { cause })
+    assertInstanceOf(t, error, cause.constructor)
+  })
+})
+
+each(getUnknownErrors(), ({ title }, cause) => {
+  test(`AnyError with unknown cause uses UnknownError | ${title}`, (t) => {
+    const error = new AnyError('message', { cause })
+    assertInstanceOf(t, error, UnknownError)
+  })
+})
+
+each(
+  [...KnownErrorClasses, ...UnknownErrorClasses],
+  [...getKnownErrors(), ...getUnknownErrors()],
+  ({ title }, ErrorClass, cause) => {
+    test(`Known class with known or unknown cause uses parent class | ${title}`, (t) => {
+      const error = new ErrorClass('message', { cause })
+      assertInstanceOf(t, error, ErrorClass)
+    })
+  },
+)
 
 test('AnyError with known cause uses its instance', (t) => {
   const cause = new TestError('causeMessage')
