6ec0e1b8a94b259fc2e4b19b551415838a9f0101
ehmicky <ehmicky@gmail.com>
2022-10-09 13:56:10 +0200

    Add more tests

--- test/plugins/error_info.js
+++ test/plugins/error_info.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import { each } from 'test-each'
 
 import { getNativeErrors } from '../helpers/known.js'
-import { defineClassOpts } from '../helpers/main.js'
+import { defineGlobalOpts, defineClassOpts } from '../helpers/main.js'
 
 const { TestError, UnknownError, AnyError } = defineClassOpts()
 
@@ -39,4 +39,29 @@ each([getSetArgs, getInstanceArgs, getStaticArgs], ({ title }, getValues) => {
     t.true(errorInfo(new UnknownError('test')).unknownDeep)
     t.false(errorInfo(new TestError('test')).unknownDeep)
   })
+
+  test(`errorInfo returns instance options | ${title}`, (t) => {
+    t.true(errorInfo(new TestError('test', { prop: true })).options.prop)
+  })
 })
+
+each(
+  [getSetArgs, getInstanceArgs, getStaticArgs],
+  [defineGlobalOpts, defineClassOpts],
+  ({ title }, getValues, defineOpts) => {
+    test(`errorInfo returns global and class options | ${title}`, (t) => {
+      const ErrorClasses = defineOpts({ prop: true })
+      const { errorInfo } = getValues(ErrorClasses)
+      t.true(errorInfo(new ErrorClasses.TestError('test')).options.prop)
+    })
+
+    test(`errorInfo global and class options have less priority than instance options | ${title}`, (t) => {
+      const ErrorClasses = defineOpts({ prop: false })
+      const { errorInfo } = getValues(ErrorClasses)
+      t.true(
+        errorInfo(new ErrorClasses.TestError('test', { prop: true })).options
+          .prop,
+      )
+    })
+  },
+)
