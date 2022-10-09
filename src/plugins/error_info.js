36e37080e272fc9eb0f988e501285e566639f7cf
ehmicky <ehmicky@gmail.com>
2022-10-09 13:54:33 +0200

    Add comment

--- src/plugins/error_info.js
+++ src/plugins/error_info.js
@@ -3,7 +3,12 @@ import { mergeClassOpts } from './merge.js'
 
 // Create `info.errorInfo(error)` which returns error-specific information:
 // `options` and `unknownDeep`.
-// This is meant to be used by plugins.
+// This is meant to be used by plugins either:
+//  - Operating on nested errors
+//  - With static methods operating on errors
+//     - E.g. when integrating with another library's plugin system, static
+//       methods are needed to return that other plugin, but they need to take
+//       errors as argument
 export const getErrorInfo = function (
   { errorData, AnyError, ErrorClasses, methodOpts, plugins, plugin },
   error,
