eed66af90243e54ddc6bbf7c24caed08b2f4f794
ehmicky <ehmicky@gmail.com>
2022-10-23 19:48:28 +0200

    Move line

--- src/types/options/plugins.ts
+++ src/types/options/plugins.ts
@@ -1,12 +1,12 @@
 import type { Plugins, Plugin } from '../plugins/main.js'
 import type { ErrorProps } from '../core_plugins/props.js'
 
-type LiteralString<T extends string> = string extends T ? never : T
-
 export type ExternalPluginOptions<PluginArg extends Plugin> =
   PluginArg['getOptions'] extends NonNullable<Plugin['getOptions']>
     ? Parameters<PluginArg['getOptions']>[0]
     : never
+
+type LiteralString<T extends string> = string extends T ? never : T
 
 type ExternalPluginsOptions<PluginsArg extends Plugins> = {
   readonly [PluginArg in PluginsArg[number] as LiteralString<
