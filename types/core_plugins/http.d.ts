3ef0762bec37bc95fd0d61116da84d7da66bed28
ehmicky <ehmicky@gmail.com>
2022-10-30 11:37:47 +0100

    Add more comments

--- types/core_plugins/http.d.ts
+++ types/core_plugins/http.d.ts
@@ -4,12 +4,42 @@ import type { Info } from '../main.js'
  * Options of `modern-errors-http`
  */
 export interface Options {
+  /**
+   * URI identifying and documenting the error class. Ideally, each error class
+   * [should set one](https://github.com/ehmicky/modern-errors/README.md#plugin-options).
+   */
   readonly type?: string
+
+  /**
+   * HTTP status code.
+   */
   readonly status?: number
+
+  /**
+   * Error class name.
+   */
   readonly title?: string
+
+  /**
+   * Error description.
+   */
   readonly detail?: string
+
+  /**
+   * URI identifying the value which errored.
+   */
   readonly instance?: string
+
+  /**
+   * Error stack trace. Can be set to an empty string.
+   */
   readonly stack?: string
+
+  /**
+   * Additional information. This is always
+   * [safe to serialize as JSON](https://github.com/ehmicky/safe-json-value).
+   * Can be set to an empty object.
+   */
   readonly extra?: object
 }
 
