ce5b28769fcb94b30ba07995d6b5668c137c44d5
ehmicky <ehmicky@gmail.com>
2022-10-30 19:41:29 +0100

    Add plugin example

---
+++ examples/plugin/main.js
@@ -0,0 +1,23 @@
+export default {
+  // Name used to configure the plugin
+  name: 'example',
+
+  // Set error properties
+  // properties() {},
+
+  // Add error instance methods like `error.exampleMethod()`
+  // instanceMethods: {
+  //   exampleMethod() {},
+  // },
+
+  // Add `AnyError` static methods like `AnyError.staticMethod()`
+  // staticMethods: {
+  //   staticMethod() {},
+  // },
+
+  // Validate and normalize options
+  // getOptions() {},
+
+  // Determine if a value is plugin's options
+  // isOptions() {},
+}
