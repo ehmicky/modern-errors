dd06e8a1ff1f309ace65fb319d9f8eeed5eb8038
ehmicky <ehmicky@gmail.com>
2022-10-30 23:25:07 +0100

    Refactoring

--- types/subclass/inherited.test-d.ts
+++ types/subclass/inherited.test-d.ts
@@ -5,32 +5,29 @@ import modernErrors from '../main.js'
 const AnyError = modernErrors()
 
 const SError = AnyError.subclass('SError')
-expectError(SError.normalize(''))
-
 const SSError = SError.subclass('SSError')
-expectError(SSError.normalize(''))
-
 const CError = AnyError.subclass('CError', {
   custom: class extends AnyError {
     static prop = true as const
   },
 })
-expectType<true>(CError.prop)
-
 const SCError = CError.subclass('SCError')
-expectType<true>(SCError.prop)
-
 const CCError = CError.subclass('CCError', {
   custom: class extends CError {
     static deepProp = true as const
   },
 })
-expectType<true>(CCError.prop)
-expectType<true>(CCError.deepProp)
-
 const CSError = SError.subclass('CSError', {
   custom: class extends SError {
     static prop = true as const
   },
 })
+
+expectType<true>(CError.prop)
+expectType<true>(SCError.prop)
+expectType<true>(CCError.prop)
+expectType<true>(CCError.deepProp)
 expectType<true>(CSError.prop)
+
+expectError(SError.normalize(''))
+expectError(SSError.normalize(''))
