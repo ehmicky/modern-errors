39b18168155506cf312e5b1f3c0f5723286cd56b
ehmicky <ehmicky@gmail.com>
2022-10-30 23:18:33 +0100

    Refactoring

--- types/subclass/parent.test-d.ts
+++ types/subclass/parent.test-d.ts
@@ -5,17 +5,7 @@ import modernErrors, { InstanceOptions } from '../main.js'
 const AnyError = modernErrors()
 
 const SError = AnyError.subclass('SError')
-const sError = new SError('')
-
-new AnyError('', { cause: sError })
-expectError(new AnyError())
-expectError(new AnyError(true))
-expectError(new AnyError('', true))
-expectError(new AnyError('', { cause: '', other: true }))
-
 const SSError = SError.subclass('SSError')
-new SSError('')
-
 const CError = AnyError.subclass('CError', {
   custom: class extends AnyError {
     constructor(
@@ -27,23 +17,7 @@ const CError = AnyError.subclass('CError', {
     }
   },
 })
-new CError('', { prop: true })
-expectError(new CError())
-expectError(new CError(true))
-expectError(new CError('', true))
-expectError(new CError('', { other: true }))
-expectError(new CError('', { cause: '', other: true }))
-expectError(new CError('', { prop: false }))
-
 const SCError = CError.subclass('SCError')
-new SCError('', { prop: true })
-expectError(new SCError())
-expectError(new SCError(true))
-expectError(new SCError('', true))
-expectError(new SCError('', { other: true }))
-expectError(new SCError('', { cause: '', other: true }))
-expectError(new SCError('', { prop: false }))
-
 const CSError = SError.subclass('CSError', {
   custom: class extends SError {
     constructor(
@@ -55,14 +29,6 @@ const CSError = SError.subclass('CSError', {
     }
   },
 })
-new CSError('', { prop: true })
-expectError(new CSError())
-expectError(new CSError(true))
-expectError(new CSError('', true))
-expectError(new CSError('', { other: true }))
-expectError(new CSError('', { cause: '', other: true }))
-expectError(new CSError('', { prop: false }))
-
 const CCError = CError.subclass('CCError', {
   custom: class extends CError {
     constructor(
@@ -74,14 +40,69 @@ const CCError = CError.subclass('CCError', {
     }
   },
 })
-new CCError('', { prop: true, propTwo: true })
+
+new AnyError('', { cause: '' })
+new SError('')
+new SSError('')
+new CError('', { prop: true }, true)
+new SCError('', { prop: true }, true)
+new CSError('', { prop: true }, true)
+new CCError('', { prop: true, propTwo: true }, false)
+
+expectError(new AnyError())
+expectError(new SError())
+expectError(new SSError())
+expectError(new CError())
+expectError(new SCError())
+expectError(new CSError())
 expectError(new CCError())
+
+expectError(new AnyError(true))
+expectError(new SError(true))
+expectError(new SSError(true))
+expectError(new CError(true))
+expectError(new SCError(true))
+expectError(new CSError(true))
 expectError(new CCError(true))
+
+expectError(new AnyError('', true))
+expectError(new SError('', true))
+expectError(new SSError('', true))
+expectError(new CError('', true))
+expectError(new SCError('', true))
+expectError(new CSError('', true))
 expectError(new CCError('', true))
+
+expectError(new SError('', { other: true }))
+expectError(new SSError('', { other: true }))
+expectError(new CError('', { other: true }))
+expectError(new SCError('', { other: true }))
+expectError(new CSError('', { other: true }))
 expectError(new CCError('', { other: true }))
+
+expectError(new AnyError('', { cause: '', other: true }))
+expectError(new SError('', { cause: '', other: true }))
+expectError(new SSError('', { cause: '', other: true }))
+expectError(new CError('', { cause: '', other: true }))
+expectError(new SCError('', { cause: '', other: true }))
+expectError(new CSError('', { cause: '', other: true }))
 expectError(new CCError('', { cause: '', other: true }))
+
+expectError(new CError('', { prop: false }))
+expectError(new SCError('', { prop: false }))
+expectError(new CSError('', { prop: false }))
 expectError(new CCError('', { prop: false }))
 expectError(new CCError('', { propTwo: false }))
+
+expectError(new CError('', {}, false))
+expectError(new SCError('', {}, false))
+expectError(new CSError('', {}, false))
+expectError(new CCError('', {}, ''))
+
+expectError(new CError('', {}, true, true))
+expectError(new SCError('', {}, true, true))
+expectError(new CSError('', {}, true, true))
+expectError(new CSError('', {}, true, true))
 
 AnyError.subclass('TestError', {
   custom: class extends AnyError {
