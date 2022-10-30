2e199e66054a1c4a3b6d4a6fb2b00d5192d707e0
ehmicky <ehmicky@gmail.com>
2022-10-30 19:47:07 +0100

    Add an example

---
+++ examples/plugin/main.test.ts
@@ -0,0 +1,9 @@
+import assert from 'assert'
+
+import modernErrors from 'modern-errors'
+import modernErrorsExample from 'modern-errors-example'
+
+const AnyError = modernErrors([modernErrorsExample])
+const error = new AnyError('', { cause: '' })
+
+assert.equal(error.exampleMethod(), 'expectedValue')
