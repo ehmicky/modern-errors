da7743aff0540895b7fd6e08cce31d1af0164531
ehmicky <ehmicky@gmail.com>
2022-11-06 19:47:39 +0100

    Add tests

--- test/plugins/shape/duplicate.js
+++ test/plugins/shape/duplicate.js
@@ -5,26 +5,45 @@ import { getClasses } from '../../helpers/main.js'
 
 const { ErrorClasses } = getClasses()
 
-each(ErrorClasses, ({ title }, ErrorClass) => {
-  test(`plugin.staticMethods cannot be defined twice by different plugins | ${title}`, (t) => {
-    t.throws(
-      ErrorClass.subclass.bind(undefined, 'TestError', {
-        plugins: [
+const definePluginsSameClass = function (ErrorClass, pluginA, pluginB) {
+  return ErrorClass.subclass.bind(undefined, 'TestError', {
+    plugins: [pluginA, pluginB],
+  })
+}
+
+const definePluginsSubClass = function (ErrorClass, pluginA, pluginB) {
+  const TestError = ErrorClass.subclass('TestError', { plugins: [pluginA] })
+  return TestError.subclass.bind(undefined, 'SubTestError', {
+    plugins: [pluginB],
+  })
+}
+
+each(
+  ErrorClasses,
+  [definePluginsSameClass, definePluginsSubClass],
+  ({ title }, ErrorClass, definePlugins) => {
+    test(`Cannot pass twice same plugins | ${title}`, (t) => {
+      t.throws(definePlugins(ErrorClass, { name: 'one' }, { name: 'one' }))
+    })
+
+    test(`plugin.staticMethods cannot be defined twice by different plugins | ${title}`, (t) => {
+      t.throws(
+        definePlugins(
+          ErrorClass,
           { name: 'one', staticMethods: { one() {} } },
           { name: 'two', staticMethods: { one() {} } },
-        ],
-      }),
-    )
-  })
+        ),
+      )
+    })
 
-  test(`plugin.instanceMethods cannot be defined twice by different plugins | ${title}`, (t) => {
-    t.throws(
-      ErrorClass.subclass.bind(undefined, 'TestError', {
-        plugins: [
+    test(`plugin.instanceMethods cannot be defined twice by different plugins | ${title}`, (t) => {
+      t.throws(
+        definePlugins(
+          ErrorClass,
           { name: 'one', instanceMethods: { one() {} } },
           { name: 'two', instanceMethods: { one() {} } },
-        ],
-      }),
-    )
-  })
-})
+        ),
+      )
+    })
+  },
+)
