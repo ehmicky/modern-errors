f6107e9e343a86afbabd76a97b50e022cb9c6a87
ehmicky <ehmicky@gmail.com>
2022-11-06 19:38:11 +0100

    Add tests

--- test/merge/cause.js
+++ test/merge/cause.js
@@ -2,7 +2,10 @@ import test from 'ava'
 import { each } from 'test-each'
 
 import { getClasses } from '../helpers/main.js'
-import { getUnknownErrors } from '../helpers/unknown.js'
+import {
+  getUnknownErrors,
+  getUnknownErrorInstances,
+} from '../helpers/unknown.js'
 
 const { ErrorClasses } = getClasses()
 
@@ -12,29 +15,28 @@ const assertInstanceOf = function (t, error, ErrorClass) {
   t.is(error.name, ErrorClass.name)
 }
 
-each(ErrorClasses, ({ title }, ErrorClass) => {
-  test(`ErrorClass with cause of same class use child class and instance | ${title}`, (t) => {
-    const cause = new ErrorClass('causeMessage')
+each(ErrorClasses, getUnknownErrors(), ({ title }, ErrorClass, getError) => {
+  test(`Unknown cause uses parent class and instance | ${title}`, (t) => {
+    const cause = getError()
     const error = new ErrorClass('message', { cause })
     assertInstanceOf(t, error, ErrorClass)
-    t.is(error, cause)
+    t.not(error, cause)
   })
+})
 
-  test(`"cause" is merged | ${title}`, (t) => {
-    const outerMessage = 'message'
-    const innerMessage = 'causeMessage'
-    const error = new ErrorClass(outerMessage, { cause: innerMessage })
-    t.false('cause' in error)
-    t.is(error.message, `${innerMessage}\n${outerMessage}`)
-  })
+each(
+  ErrorClasses,
+  getUnknownErrorInstances(),
+  ({ title }, ErrorClass, getError) => {
+    test(`Unknown causes are merged | ${title}`, (t) => {
+      const error = getError()
+      error.one = true
+      t.true(new ErrorClass('message', { cause: error }).one)
+    })
+  },
+)
 
-  test(`"cause" is ignored if undefined | ${title}`, (t) => {
-    const outerMessage = 'message'
-    const error = new ErrorClass(outerMessage, { cause: undefined })
-    t.false('cause' in error)
-    t.is(error.message, outerMessage)
-  })
-
+each(ErrorClasses, ({ title }, ErrorClass) => {
   test(`ErrorClass with cause of subclass use child class and instance | ${title}`, (t) => {
     const TestError = ErrorClass.subclass('TestError')
     const cause = new TestError('causeMessage')
@@ -43,20 +45,45 @@ each(ErrorClasses, ({ title }, ErrorClass) => {
     t.is(error, cause)
   })
 
-  test(`ErrorClass with cause of superclass use parent class | ${title}`, (t) => {
+  test(`ErrorClass with cause of same class use child class and instance | ${title}`, (t) => {
+    const cause = new ErrorClass('causeMessage')
+    const error = new ErrorClass('message', { cause })
+    assertInstanceOf(t, error, ErrorClass)
+    t.is(error, cause)
+  })
+
+  test(`ErrorClass with cause of superclass use parent class and instance | ${title}`, (t) => {
     const TestError = ErrorClass.subclass('TestError')
     const cause = new ErrorClass('causeMessage')
     const error = new TestError('message', { cause })
     assertInstanceOf(t, error, TestError)
     t.not(error, cause)
   })
-})
 
-each(ErrorClasses, getUnknownErrors(), ({ title }, ErrorClass, getError) => {
-  test(`Unknown cause uses parent class and instance | ${title}`, (t) => {
-    const cause = getError()
-    const error = new ErrorClass('message', { cause })
-    assertInstanceOf(t, error, ErrorClass)
-    t.not(error, cause)
+  test(`"cause" property is not left | ${title}`, (t) => {
+    t.false('cause' in new ErrorClass('message', { cause: '' }))
+  })
+
+  test(`"cause" message is merged | ${title}`, (t) => {
+    const outerMessage = 'message'
+    const innerMessage = 'causeMessage'
+    const error = new ErrorClass(outerMessage, { cause: innerMessage })
+    t.false('cause' in error)
+    t.is(error.message, `${innerMessage}\n${outerMessage}`)
+  })
+
+  test(`"cause" properties are merged | ${title}`, (t) => {
+    const cause = new ErrorClass('causeMessage', { props: { one: true } })
+    const error = new ErrorClass('message', { cause, props: { two: true } })
+    t.true(error.one)
+    t.true(error.two)
+  })
+
+  test(`"cause" is ignored if undefined | ${title}`, (t) => {
+    const outerMessage = 'message'
+    t.is(
+      new ErrorClass(outerMessage, { cause: undefined }).message,
+      outerMessage,
+    )
   })
 })
