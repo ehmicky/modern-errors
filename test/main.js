ad245ddec643c8a911217708985d4318c6a6ed1e
ehmicky <ehmicky@gmail.com>
2022-11-06 17:48:45 +0100

    Fix tests

--- test/main.js
+++ test/main.js
@@ -1,20 +1,12 @@
 import test from 'ava'
 import { each } from 'test-each'
 
-import { defineClassOpts } from './helpers/main.js'
+import { KnownErrorClasses } from './helpers/known.js'
 
-const { TestError, UnknownError } = defineClassOpts()
-
-each([TestError, UnknownError], ({ title }, ErrorClass) => {
+each(KnownErrorClasses, ({ title }, ErrorClass) => {
   test(`Can use known error classes | ${title}`, (t) => {
     const error = new ErrorClass('test')
     t.true(error instanceof ErrorClass)
     t.true(error instanceof Error)
   })
-})
-
-test('Can be called several times', (t) => {
-  const { TestError: OtherTestError } = defineClassOpts()
-  const error = new OtherTestError('test')
-  t.true(error instanceof OtherTestError)
 })
