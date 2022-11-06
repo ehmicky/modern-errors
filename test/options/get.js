23544a5f477950d2cdc22a164fd325de910b6849
ehmicky <ehmicky@gmail.com>
2022-11-06 17:59:28 +0100

    Fix tests

--- test/options/get.js
+++ test/options/get.js
@@ -1,34 +1,46 @@
 import test from 'ava'
 import { each } from 'test-each'
 
-import { defineGlobalOpts, defineClassOpts } from '../helpers/main.js'
-import { TEST_PLUGIN } from '../helpers/plugin.js'
+import { KnownErrorClasses, SpecificErrorClasses } from '../helpers/known.js'
 
-const { TestError } = defineClassOpts()
-const { TestError: NoOptsTestError } = defineClassOpts({}, {}, [
-  { ...TEST_PLUGIN, getOptions: undefined },
-])
+const properties = () => ({})
 
-test('plugin.getOptions() forbids options by default', (t) => {
-  t.throws(() => new NoOptsTestError('test', { prop: true }))
-})
-
-each([undefined, {}, { prop: undefined }], ({ title }, opts) => {
-  test(`plugin.getOptions() allows undefined options by default | ${title}`, (t) => {
-    t.is(new NoOptsTestError('test', opts).properties.options, undefined)
+each(KnownErrorClasses, ({ title }, ErrorClass) => {
+  test(`plugin.getOptions() forbids options by default | ${title}`, (t) => {
+    const TestError = ErrorClass.subclass('TestError', {
+      plugins: [{ name: 'one', properties }],
+    })
+    // eslint-disable-next-line max-nested-callbacks
+    t.throws(() => new TestError('test', { one: true }))
   })
 })
 
-each([defineGlobalOpts, defineClassOpts], ({ title }, defineOpts) => {
-  test(`plugin.getOptions() exceptions are thrown right away for global and class options | ${title}`, (t) => {
-    t.throws(defineOpts.bind(undefined, { prop: 'invalid' }), {
+each(
+  KnownErrorClasses,
+  [undefined, {}, { one: undefined }],
+  ({ title }, ErrorClass, opts) => {
+    test(`plugin.getOptions() allows undefined options by default | ${title}`, (t) => {
+      const TestError = ErrorClass.subclass('TestError', {
+        plugins: [{ name: 'one', properties }],
+      })
+      // eslint-disable-next-line max-nested-callbacks
+      t.notThrows(() => new TestError('test', opts))
+    })
+  },
+)
+
+each(SpecificErrorClasses, ({ title }, ErrorClass) => {
+  test(`plugin.getOptions() validate class options | ${title}`, (t) => {
+    t.throws(
+      ErrorClass.subclass.bind(undefined, 'TestError', { prop: 'invalid' }),
+      { message: 'Invalid "prop" options: Invalid prop' },
+    )
+  })
+
+  test(`plugin.getOptions() validate instance options | ${title}`, (t) => {
+    // eslint-disable-next-line max-nested-callbacks
+    t.throws(() => new ErrorClass('test', { prop: 'invalid' }), {
       message: 'Invalid "prop" options: Invalid prop',
     })
-  })
-})
-
-test('plugin.getOptions() exceptions are not thrown right away for instance options', (t) => {
-  t.throws(() => new TestError('test', { prop: 'invalid' }), {
-    message: 'Invalid "prop" options: Invalid prop',
   })
 })
