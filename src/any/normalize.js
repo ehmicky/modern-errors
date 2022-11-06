c20ea6a5ffc1f3e0725851ce60992c2f17aceee8
ehmicky <ehmicky@gmail.com>
2022-11-06 15:11:45 +0100

    Add comments

--- src/any/normalize.js
+++ src/any/normalize.js
@@ -3,13 +3,23 @@ import normalizeException from 'normalize-exception'
 // This has two purposes:
 //  - Normalizing exceptions:
 //     - Inside any `catch` block
-//     - To error instances with normal properties
-//     - To instances of `AnyError` (or any subclass)
+//     - To:
+//        - Error instances with normal properties
+//        - Instances of `AnyError` (or any subclass), so plugin instance
+//          methods can be called
 //     - This is meant to be called as `AnyError.normalize()`
 //  - Assigning a default class:
 //     - Inside a top-level `catch` block
-//     - For errors that are not instances of `AnyError` (or any subclass)
+//     - For errors that are generic, either:
+//        - Not `AnyError` instances
+//        - `AnyError` itself
 //     - This is meant to be called as `UnknownError.normalize()`
+// `AnyError` is meant as a placeholder class
+//  - It should be replaced by a subclass in a parent `catch` block
+//  - This is useful when wrapping or normalizing errors
+//  - By opposition, `UnknownError.normalize()` should be top-level, to ensure
+//    no parent `catch` block changes the class to a more precise one, since
+//    this would keep `UnknownError` class options
 // This returns the `error` instead of throwing it so the user can handle it
 // before re-throwing it if needed.
 // This is called `normalize()`, not `normalizeError()` so it does not end
