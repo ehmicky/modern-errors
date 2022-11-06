49e79ed38c10ac5a98bcc2027455a3237de53f55
ehmicky <ehmicky@gmail.com>
2022-11-06 17:48:47 +0100

    Merge files

--- test/main.js
+++ test/main.js
@@ -1,7 +1,13 @@
 import test from 'ava'
 import { each } from 'test-each'
 
-import { KnownErrorClasses } from './helpers/known.js'
+import {
+  KnownErrorClasses,
+  SpecificErrorClasses,
+  getUnknownErrors,
+  ModernError,
+  AnyError,
+} from './helpers/known.js'
 
 each(KnownErrorClasses, ({ title }, ErrorClass) => {
   test(`Can use known error classes | ${title}`, (t) => {
@@ -9,4 +15,28 @@ each(KnownErrorClasses, ({ title }, ErrorClass) => {
     t.true(error instanceof ErrorClass)
     t.true(error instanceof Error)
   })
+})
+
+each(SpecificErrorClasses, ({ title }, ErrorClass) => {
+  test(`instanceof AnyError can be used with known errors | ${title}`, (t) => {
+    t.true(new ErrorClass('test') instanceof AnyError)
+  })
+})
+
+each(getUnknownErrors(), ({ title }, getUnknownError) => {
+  test(`instanceof AnyError can be used with known errors | ${title}`, (t) => {
+    t.false(getUnknownError() instanceof AnyError)
+  })
+})
+
+test('instanceof AnyError prevents naming collisions', (t) => {
+  const OtherAnyError = ModernError.subclass('OtherAnyError')
+  t.false(new OtherAnyError('test') instanceof AnyError)
+})
+
+test('AnyError.prototype.name is correct', (t) => {
+  t.is(AnyError.prototype.name, 'AnyError')
+  t.false(
+    Object.getOwnPropertyDescriptor(AnyError.prototype, 'name').enumerable,
+  )
 })
