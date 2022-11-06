1f3cd8ae339b5481bdfcd76b2253b4b7b986feec
ehmicky <ehmicky@gmail.com>
2022-11-06 15:23:33 +0100

    Improve tests

--- test/any/aggregate.js
+++ test/any/aggregate.js
@@ -1,48 +1,41 @@
 import test from 'ava'
 import { each } from 'test-each'
 
-import { defineClassOpts } from '../helpers/main.js'
+import { AnyError, TestError, KnownErrorClasses } from '../helpers/known.js'
 
-const { TestError, AnyError } = defineClassOpts()
-
-test('error.errors can be set', (t) => {
-  t.deepEqual(new TestError('test', { errors: [] }).errors, [])
-})
-
-each([undefined, {}, { errors: undefined }], ({ title }, opts) => {
-  test(`error.errors is not set by default | ${title}`, (t) => {
-    t.false('errors' in new TestError('test', opts))
+each(KnownErrorClasses, ({ title }, ErrorClass) => {
+  test(`error.errors can be set | ${title}`, (t) => {
+    t.deepEqual(new ErrorClass('test', { errors: [] }).errors, [])
   })
-})
 
-test('error.errors is validated', (t) => {
-  t.throws(() => new TestError('test', { errors: true }))
-})
+  test(`error.errors is validated | ${title}`, (t) => {
+    // eslint-disable-next-line max-nested-callbacks
+    t.throws(() => new ErrorClass('test', { errors: true }))
+  })
 
-test('error.errors is not enumerable', (t) => {
-  t.false(
-    Object.getOwnPropertyDescriptor(
-      new TestError('test', { errors: [] }),
-      'errors',
-    ).enumerable,
-  )
-})
+  test(`error.errors is not enumerable | ${title}`, (t) => {
+    t.false(
+      Object.getOwnPropertyDescriptor(
+        new ErrorClass('test', { errors: [] }),
+        'errors',
+      ).enumerable,
+    )
+  })
 
-test('error.errors are normalized', (t) => {
-  t.true(
-    new TestError('test', { errors: [true] }).errors[0] instanceof AnyError,
-  )
-})
+  test(`error.errors are normalized | ${title}`, (t) => {
+    t.true(
+      new ErrorClass('test', { errors: [true] }).errors[0] instanceof AnyError,
+    )
+  })
 
-test('error.errors are normalized deeply', (t) => {
-  const cause = new Error('causeMessage')
-  // eslint-disable-next-line fp/no-mutation
-  cause.errors = ['one']
-  const { errors } = new TestError('test', { cause })
-  t.true(errors[0] instanceof AnyError)
-})
+  test(`error.errors are normalized deeply | ${title}`, (t) => {
+    const cause = new Error('causeMessage')
+    // eslint-disable-next-line fp/no-mutation
+    cause.errors = ['one']
+    const { errors } = new ErrorClass('test', { cause })
+    t.true(errors[0] instanceof AnyError)
+  })
 
-each([TestError, AnyError], ({ title }, ErrorClass) => {
   test(`error.errors are appended to | ${title}`, (t) => {
     const one = new TestError('one')
     const two = new TestError('two')
@@ -51,3 +44,13 @@ each([TestError, AnyError], ({ title }, ErrorClass) => {
     t.deepEqual(error.errors, [one, two])
   })
 })
+
+each(
+  KnownErrorClasses,
+  [undefined, {}, { errors: undefined }],
+  ({ title }, ErrorClass, opts) => {
+    test(`error.errors is not set by default | ${title}`, (t) => {
+      t.false('errors' in new ErrorClass('test', opts))
+    })
+  },
+)
