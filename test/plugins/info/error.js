35dd5875aafcaf7c9a99511770ce65593879ca4b
ehmicky <ehmicky@gmail.com>
2022-11-06 19:17:02 +0100

    Fix tests

--- test/plugins/info/error.js
+++ test/plugins/info/error.js
@@ -41,3 +41,59 @@ each(
     })
   },
 )
+
+each(
+  ErrorSubclasses,
+  [getPropertiesInfo, getInstanceInfo, getStaticInfo],
+  ({ title }, ErrorClass, getValues) => {
+    test(`errorInfo returns instance options | ${title}`, (t) => {
+      const { errorInfo } = getValues(ErrorClass)
+      t.true(errorInfo(new ErrorClass('test', { prop: true })).options.prop)
+    })
+
+    test(`errorInfo returns class options | ${title}`, (t) => {
+      const TestError = ErrorClass.subclass('TestError', { prop: true })
+      const { errorInfo } = getValues(TestError)
+      t.true(errorInfo(new TestError('test')).options.prop)
+    })
+
+    test(`errorInfo class options have less priority than instance options | ${title}`, (t) => {
+      const TestError = ErrorClass.subclass('TestError', { prop: true })
+      const { errorInfo } = getValues(TestError)
+      t.false(errorInfo(new TestError('test', { prop: false })).options.prop)
+    })
+  },
+)
+
+each(
+  ErrorSubclasses,
+  [getPropertiesInfo, getInstanceInfo],
+  ({ title }, ErrorClass, getValues) => {
+    test(`errorInfo ignores parent instance options | ${title}`, (t) => {
+      const { errorInfo } = getValues(ErrorClass, { prop: true })
+      t.is(errorInfo(new ErrorClass('test')).options.prop, undefined)
+    })
+  },
+)
+
+each(
+  ErrorSubclasses,
+  [getInstanceInfo, getStaticInfo],
+  ({ title }, ErrorClass, getValues) => {
+    test(`errorInfo returns method options | ${title}`, (t) => {
+      const { errorInfo } = getValues(ErrorClass, {}, true)
+      t.true(errorInfo(new ErrorClass('test')).options.prop)
+    })
+
+    test(`errorInfo method options have more priority than instance options | ${title}`, (t) => {
+      const { errorInfo } = getValues(ErrorClass, {}, true)
+      t.true(errorInfo(new ErrorClass('test', { prop: false })).options.prop)
+    })
+
+    test(`errorInfo global and class options have less priority than method options | ${title}`, (t) => {
+      const TestError = ErrorClass.subclass('TestError', { prop: false })
+      const { errorInfo } = getValues(TestError, {}, true)
+      t.true(errorInfo(new TestError('test')).options.prop)
+    })
+  },
+)
