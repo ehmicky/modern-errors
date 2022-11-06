18fb235c8d69d2fe7801b6d10ae38482739a42e3
ehmicky <ehmicky@gmail.com>
2022-11-06 19:19:48 +0100

    Move comment

--- src/main.js
+++ src/main.js
@@ -10,25 +10,6 @@ import { classesData, instancesData } from './subclass/map.js'
 import { validateSubclass } from './subclass/validate.js'
 
 // Base class for all error classes.
-// We encourage `instanceof` over `error.name` for checking since this:
-//  - Prevents name collisions with other libraries
-//  - Allows checking if any error came from a given library
-//  - Includes error classes in the exported interface explicitly instead of
-//    implicitly, so that users are mindful about breaking changes
-//  - Bundles classes with TypeScript documentation, types and autocompletion
-//  - Encourages documenting error types
-// Checking class with `error.name` is still supported, but not documented
-//  - Since it is widely used and can be better in specific cases
-//  - It also does not have narrowing with TypeScript
-// This also provides with namespacing, i.e. prevents classes of the same name
-// but in different libraries to be considered equal. As opposed to the
-// following alternatives:
-//  - Namespacing all error names with a common prefix since this:
-//     - Leads to verbose error names
-//     - Requires either an additional option, or guessing ambiguously whether
-//       error names are meant to include a namespace prefix
-//  - Using a separate `namespace` property: this adds too much complexity and
-//    is less standard than `instanceof`
 // We do not call `Error.captureStackTrace(this, CustomErrorClass)` because:
 //  - It is V8 specific
 //  - And on V8 (unlike in some browsers like Firefox), `Error.stack`
--- src/utils/subclass.js
+++ src/utils/subclass.js
@@ -1,4 +1,22 @@
-// Check if `ErrorClass` is a subclass of `ParentClass`
+// Check if `ErrorClass` is a subclass of `ParentClass`.
+// We encourage `instanceof` over `error.name` for checking since this:
+//  - Prevents name collisions with other libraries
+//  - Allows checking if any error came from a given library
+//  - Includes error classes in the exported interface explicitly instead of
+//    implicitly, so that users are mindful about breaking changes
+//  - Bundles classes with TypeScript documentation, types and autocompletion
+//  - Encourages documenting error types
+// Checking class with `error.name` is still supported, but not documented
+//  - Since it is widely used and can be better in specific cases
+// This also provides with namespacing, i.e. prevents classes of the same name
+// but in different libraries to be considered equal. As opposed to the
+// following alternatives:
+//  - Namespacing all error names with a common prefix since this:
+//     - Leads to verbose error names
+//     - Requires either an additional option, or guessing ambiguously whether
+//       error names are meant to include a namespace prefix
+//  - Using a separate `namespace` property: this adds too much complexity and
+//    is less standard than `instanceof`
 export const isSubclass = function (ErrorClass, ParentClass) {
   return ParentClass === ErrorClass || isProtoOf.call(ParentClass, ErrorClass)
 }
