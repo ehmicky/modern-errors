1a1d18404c7a05090c3c85692e6429a086f6cab1
ehmicky <ehmicky@gmail.com>
2022-11-06 17:53:27 +0100

    Fix tests

--- test/subclass/name.js
+++ test/subclass/name.js
@@ -1,55 +1,38 @@
 import test from 'ava'
 import { each } from 'test-each'
 
-import {
-  createAnyError,
-  defineClassOpts,
-  defineSimpleCustom,
-  defineDeepCustom,
-} from '../helpers/main.js'
+import { KnownErrorClasses } from '../helpers/known.js'
 
-const { TestError, AnyError } = defineClassOpts()
-const { TestError: DeepCustomError } = defineDeepCustom()
-const { SimpleCustomError } = defineSimpleCustom()
+each(KnownErrorClasses, ({ title }, ErrorClass) => {
+  test(`Allows duplicate names | ${title}`, (t) => {
+    const DuplicateClass = ErrorClass.subclass(ErrorClass.name)
+    t.is(DuplicateClass.name, ErrorClass.name)
+  })
 
-test('Validate against duplicate names', (t) => {
-  t.throws(AnyError.subclass.bind(undefined, 'TestError'))
+  test(`prototype.name is correct | ${title}`, (t) => {
+    t.is(ErrorClass.prototype.name, ErrorClass.name)
+    t.false(
+      Object.getOwnPropertyDescriptor(ErrorClass.prototype, 'name').enumerable,
+    )
+    t.is(new ErrorClass('test').name, ErrorClass.name)
+  })
 })
 
 each(
+  KnownErrorClasses,
   [
     undefined,
     '',
     {},
-    'AnyError',
     'Error',
     'TypeError',
     'inputError',
     'input_error',
     'input',
   ],
-  ({ title }, errorName) => {
+  ({ title }, ErrorClass, errorName) => {
     test(`Validate invalid error name | ${title}`, (t) => {
-      t.throws(AnyError.subclass.bind(undefined, errorName))
+      t.throws(ErrorClass.subclass.bind(undefined, errorName))
     })
   },
 )
-
-each(
-  [TestError, SimpleCustomError, DeepCustomError],
-  ({ title }, ErrorClass) => {
-    test(`prototype.name is correct | ${title}`, (t) => {
-      t.is(ErrorClass.prototype.name, ErrorClass.name)
-      t.false(
-        Object.getOwnPropertyDescriptor(ErrorClass.prototype, 'name')
-          .enumerable,
-      )
-      t.is(new ErrorClass('test').name, ErrorClass.name)
-    })
-  },
-)
-
-test('Require defining UnknownError before creating errors', (t) => {
-  const TestAnyError = createAnyError()
-  t.throws(TestAnyError.subclass.bind(undefined, 'TestError'))
-})
