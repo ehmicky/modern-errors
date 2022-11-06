d4069a93af89c71a26500a90f7c7c2b95cdf87e6
ehmicky <ehmicky@gmail.com>
2022-11-06 17:57:02 +0100

    Merge files

--- test/subclass/create.js
+++ test/subclass/create.js
@@ -13,4 +13,36 @@ each(KnownErrorClasses, ({ title }, ErrorClass) => {
   test(`Static methods are not enumerable | ${title}`, (t) => {
     t.deepEqual(Object.keys(ErrorClass), [])
   })
+
+  test(`Allows duplicate names | ${title}`, (t) => {
+    const DuplicateClass = ErrorClass.subclass(ErrorClass.name)
+    t.is(DuplicateClass.name, ErrorClass.name)
+  })
+
+  test(`prototype.name is correct | ${title}`, (t) => {
+    t.is(ErrorClass.prototype.name, ErrorClass.name)
+    t.false(
+      Object.getOwnPropertyDescriptor(ErrorClass.prototype, 'name').enumerable,
+    )
+    t.is(new ErrorClass('test').name, ErrorClass.name)
+  })
 })
+
+each(
+  KnownErrorClasses,
+  [
+    undefined,
+    '',
+    {},
+    'Error',
+    'TypeError',
+    'inputError',
+    'input_error',
+    'input',
+  ],
+  ({ title }, ErrorClass, errorName) => {
+    test(`Validate invalid error name | ${title}`, (t) => {
+      t.throws(ErrorClass.subclass.bind(undefined, errorName))
+    })
+  },
+)
