8fe9d0ad5a26c5c544e53510ce79d6ea74d4184e
ehmicky <ehmicky@gmail.com>
2022-11-06 19:17:47 +0100

    Fix tests

--- test/plugins/core/props.js
+++ test/plugins/core/props.js
@@ -1,76 +1,57 @@
 import test from 'ava'
+import { each } from 'test-each'
 
-import { defineClassOpts } from '../helpers/main.js'
+import { getClasses } from '../../helpers/main.js'
+import { TEST_PLUGIN } from '../../helpers/plugin.js'
 
-const { TestError, AnyError } = defineClassOpts()
+const { ErrorClasses } = getClasses()
 
-test('"props" are validated', (t) => {
-  t.throws(() => new TestError('message', { props: true }))
-})
-
-test('"props" are assigned', (t) => {
-  t.true(new TestError('message', { props: { prop: true } }).prop)
-})
-
-test('"props" are sanitized', (t) => {
-  const message = 'message'
-  t.is(
-    new TestError(message, { props: { message: 'otherMessage' } }).message,
-    message,
-  )
-})
-
-test('"props" have priority over cause', (t) => {
-  const cause = new TestError('causeMessage', { props: { prop: false } })
-  t.true(new TestError('message', { cause, props: { prop: true } }).prop)
-})
-
-test('"props" can be used even if cause has none', (t) => {
-  const cause = new TestError('causeMessage')
-  t.true(new TestError('message', { cause, props: { prop: true } }).prop)
-})
-
-const getPropsMergeError = function (ErrorClass) {
-  const propSym = Symbol('prop')
-  const cause = new TestError('causeMessage', {
-    props: { one: true, [propSym]: true },
+each(ErrorClasses, ({ title }, ErrorClass) => {
+  test(`"props" are validated | ${title}`, (t) => {
+    // eslint-disable-next-line max-nested-callbacks
+    t.throws(() => new ErrorClass('message', { props: true }))
   })
-  const error = new ErrorClass('message', { cause, props: { two: true } })
-  return { error, propSym }
-}
 
-test('"props" are overridden without AnyError', (t) => {
-  const { error, propSym } = getPropsMergeError(TestError)
-  t.false('one' in error)
-  t.false(propSym in error)
-  t.true(error.two)
-})
+  test(`"props" are assigned | ${title}`, (t) => {
+    t.true(new ErrorClass('message', { props: { prop: true } }).prop)
+  })
 
-test('"props" are merged with AnyError', (t) => {
-  const { error, propSym } = getPropsMergeError(AnyError)
-  t.true(error.one)
-  t.true(error[propSym])
-  t.true(error.two)
-})
+  test(`"props" have priority over cause | ${title}`, (t) => {
+    const cause = new ErrorClass('causeMessage', { props: { prop: false } })
+    t.true(new ErrorClass('message', { cause, props: { prop: true } }).prop)
+  })
 
-test('"props" cannot override "message"', (t) => {
-  const message = 'testMessage'
-  const error = new TestError('', { props: { message } })
-  t.false(error.message.includes(message))
-})
+  test(`"props" can be used even if cause has none | ${title}`, (t) => {
+    const cause = new ErrorClass('causeMessage')
+    t.true(new ErrorClass('message', { cause, props: { prop: true } }).prop)
+  })
 
-test('"props" cannot unset "message"', (t) => {
-  const message = 'testMessage'
-  const cause = new TestError('', { props: { message: 'causeMessage' } })
-  const error = new TestError(message, { cause })
-  t.is(error.message, message)
-})
+  test(`"props" are merged with cause' | ${title}`, (t) => {
+    const propSym = Symbol('prop')
+    const cause = new ErrorClass('causeMessage', {
+      props: { one: true, [propSym]: true },
+    })
+    const error = new ErrorClass('message', { cause, props: { two: true } })
+    t.true(error.one)
+    t.true(error[propSym])
+    t.true(error.two)
+  })
 
-test('"props" have less priority than other plugin.properties()', (t) => {
-  t.true(
-    new TestError('message', {
-      prop: { toSet: { prop: true } },
-      props: { prop: false },
-    }).prop,
-  )
+  test(`"props" cannot override "message" | ${title}`, (t) => {
+    const message = 'testMessage'
+    const error = new ErrorClass('', { props: { message } })
+    t.false(error.message.includes(message))
+  })
+
+  test(`"props" have less priority than other plugin.properties() | ${title}`, (t) => {
+    const TestError = ErrorClass.subclass('TestError', {
+      plugins: [TEST_PLUGIN],
+    })
+    t.true(
+      new TestError('message', {
+        prop: { toSet: { prop: true } },
+        props: { prop: false },
+      }).prop,
+    )
+  })
 })
