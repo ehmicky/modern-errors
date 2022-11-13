9b968773d477169231f4e4dc4fdaaa98a1917ec6
ehmicky <ehmicky@gmail.com>
2022-11-13 13:54:19 +0100

    Improve types

--- types/options/plugins.test-d.ts
+++ types/options/plugins.test-d.ts
@@ -15,24 +15,36 @@ const BaseError = ModernError.subclass('BaseError', { plugins: [fullPlugin] })
 expectError(
   ModernError.subclass('TestError', { plugins: [barePlugin], test: true }),
 )
-
 ModernError.subclass('TestError', { plugins: [fullPlugin], test: true })
 BaseError.subclass('TestError', { test: true })
 new BaseError('', { test: true })
+expectError<ClassOptions>({ test: true })
 expectAssignable<ClassOptions<[typeof fullPlugin]>>({ test: true })
+expectError<InstanceOptions>({ test: true })
 expectAssignable<InstanceOptions<[typeof fullPlugin]>>({ test: true })
 expectAssignable<MethodOptions<typeof fullPlugin>>(true)
 
+expectError(
+  ModernError.subclass('TestError', { plugins: [barePlugin], test: 'true' }),
+)
 expectError(
   ModernError.subclass('TestError', { plugins: [fullPlugin], test: 'true' }),
 )
 expectError(BaseError.subclass('TestError', { test: 'true' }))
 expectError(new BaseError('', { test: 'true' }))
+expectNotAssignable<ClassOptions>({ test: 'true' })
 expectNotAssignable<ClassOptions<[typeof fullPlugin]>>({ test: 'true' })
+expectNotAssignable<InstanceOptions>({ test: 'true' })
 expectNotAssignable<InstanceOptions<[typeof fullPlugin]>>({ test: 'true' })
 expectNotAssignable<MethodOptions<typeof fullPlugin>>('true')
 
 expectError(ModernError.subclass('TestError', { other: true }))
+expectError(
+  ModernError.subclass('TestError', { plugins: [barePlugin], other: true }),
+)
+expectError(
+  ModernError.subclass('TestError', { plugins: [fullPlugin], other: true }),
+)
 expectError(
   ModernError.subclass('TestError', {
     plugins: [fullPlugin as Plugin],
@@ -47,12 +59,22 @@ expectError(
 )
 expectError(new BaseError('', { other: true }))
 expectNotAssignable<ClassOptions>({ other: true })
+expectNotAssignable<ClassOptions<[typeof fullPlugin]>>({ other: true })
 expectNotAssignable<InstanceOptions>({ other: true })
+expectNotAssignable<InstanceOptions<[typeof fullPlugin]>>({ other: true })
 
-expectAssignable<InstanceOptions>({})
+ModernError.subclass('TestError', {})
+ModernError.subclass('TestError', { plugins: [barePlugin] })
+ModernError.subclass('TestError', { plugins: [fullPlugin] })
+BaseError.subclass('TestError', {})
 expectAssignable<ClassOptions>({})
+expectAssignable<ClassOptions<[typeof fullPlugin]>>({})
+expectAssignable<InstanceOptions>({})
+expectAssignable<InstanceOptions<[typeof fullPlugin]>>({})
 expectAssignable<MethodOptions<typeof barePlugin>>({} as never)
 
-expectNotAssignable<InstanceOptions>(true)
 expectNotAssignable<ClassOptions>(true)
+expectNotAssignable<ClassOptions<[typeof fullPlugin]>>(true)
+expectNotAssignable<InstanceOptions>(true)
+expectNotAssignable<InstanceOptions<[typeof fullPlugin]>>(true)
 expectNotAssignable<MethodOptions<typeof barePlugin>>(true)
