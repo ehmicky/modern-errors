cd22ac740ee17a5bae1dbfcee8925e884283a85e
ehmicky <ehmicky@gmail.com>
2022-11-15 21:54:53 +0100

    Revert "Improve types"
    This reverts commit c7fbb06eeee98412f3971a370708716aa4dce9e7.

--- types/subclass/main/main.d.ts
+++ types/subclass/main/main.d.ts
@@ -11,6 +11,7 @@ import type {
 } from '../../core_plugins/props/main.js'
 import type { SpecificClassOptions } from '../../options/class.js'
 import type { Cause } from '../../options/instance.js'
+import type { OmitKeys } from '../../utils.js'
 import type {
   ErrorConstructor,
   ParentInstanceOptions,
@@ -111,7 +112,7 @@ export type SpecificErrorClass<
   ErrorPropsArg extends ErrorProps,
   CustomClass extends ErrorConstructor,
 > = ErrorSubclassCore<PluginsArg, ErrorPropsArg, CustomClass> &
-  Omit<
+  OmitKeys<
     CustomClass,
     keyof ErrorSubclassCore<PluginsArg, ErrorPropsArg, CustomClass>
   > &
