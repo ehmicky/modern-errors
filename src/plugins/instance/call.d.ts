fb8e9b5e66ec5861be1db35dfd74da47fca4be61
ehmicky <ehmicky@gmail.com>
2022-11-27 12:01:14 +0100

    Split file

---
+++ src/plugins/instance/call.d.ts
@@ -0,0 +1,59 @@
+import type { MethodOptions } from '../../options/method.js'
+import type { UnionToIntersection } from '../../utils/intersect.js'
+import type { SliceFirst } from '../../utils/slice.js'
+import type { Plugin, Plugins } from '../shape/main.js'
+
+import type { InstanceMethod, InstanceMethods } from './main.js'
+
+/**
+ * Bound instance method parameters
+ */
+type ErrorInstanceMethodParams<
+  InstanceMethodArg extends InstanceMethod,
+  MethodOptionsArg extends MethodOptions<Plugin>,
+> =
+  | SliceFirst<Parameters<InstanceMethodArg>>
+  | ([MethodOptionsArg] extends [never]
+      ? never
+      : readonly [
+          ...SliceFirst<Parameters<InstanceMethodArg>>,
+          MethodOptionsArg,
+        ])
+
+/**
+ * Bound instance method of a plugin
+ */
+type ErrorInstanceMethod<
+  InstanceMethodArg extends InstanceMethod,
+  MethodOptionsArg extends MethodOptions<Plugin>,
+> = (
+  ...args: ErrorInstanceMethodParams<InstanceMethodArg, MethodOptionsArg>
+) => ReturnType<InstanceMethodArg>
+
+/**
+ * Bound instance methods of a plugin, always defined
+ */
+type ErrorInstanceMethods<
+  InstanceMethodsArg extends InstanceMethods,
+  MethodOptionsArg extends MethodOptions<Plugin>,
+> = {
+  readonly [MethodName in keyof InstanceMethodsArg]: ErrorInstanceMethod<
+    InstanceMethodsArg[MethodName],
+    MethodOptionsArg
+  >
+}
+
+/**
+ * Bound instance methods of a plugin, if defined
+ */
+type PluginInstanceMethods<PluginArg extends Plugin> = PluginArg extends {
+  instanceMethods: InstanceMethods
+}
+  ? ErrorInstanceMethods<PluginArg['instanceMethods'], MethodOptions<PluginArg>>
+  : {}
+
+/**
+ * Bound instance methods of all plugins
+ */
+export type PluginsInstanceMethods<PluginsArg extends Plugins> =
+  UnionToIntersection<PluginInstanceMethods<PluginsArg[number]>> & {}
--- src/plugins/instance/main.d.ts
+++ src/plugins/instance/main.d.ts
@@ -1,8 +1,4 @@
-import type { MethodOptions } from '../../options/method.js'
-import type { UnionToIntersection } from '../../utils/intersect.js'
-import type { SliceFirst } from '../../utils/slice.js'
 import type { InfoParameter } from '../info/main.js'
-import type { Plugin, Plugins } from '../shape/main.js'
 
 /**
  * Unbound instance method of a plugin
@@ -18,56 +14,3 @@ export type InstanceMethod = (
 export interface InstanceMethods {
   readonly [MethodName: string]: InstanceMethod
 }
-
-/**
- * Bound instance method parameters
- */
-type ErrorInstanceMethodParams<
-  InstanceMethodArg extends InstanceMethod,
-  MethodOptionsArg extends MethodOptions<Plugin>,
-> =
-  | SliceFirst<Parameters<InstanceMethodArg>>
-  | ([MethodOptionsArg] extends [never]
-      ? never
-      : readonly [
-          ...SliceFirst<Parameters<InstanceMethodArg>>,
-          MethodOptionsArg,
-        ])
-
-/**
- * Bound instance method of a plugin
- */
-type ErrorInstanceMethod<
-  InstanceMethodArg extends InstanceMethod,
-  MethodOptionsArg extends MethodOptions<Plugin>,
-> = (
-  ...args: ErrorInstanceMethodParams<InstanceMethodArg, MethodOptionsArg>
-) => ReturnType<InstanceMethodArg>
-
-/**
- * Bound instance methods of a plugin, always defined
- */
-type ErrorInstanceMethods<
-  InstanceMethodsArg extends InstanceMethods,
-  MethodOptionsArg extends MethodOptions<Plugin>,
-> = {
-  readonly [MethodName in keyof InstanceMethodsArg]: ErrorInstanceMethod<
-    InstanceMethodsArg[MethodName],
-    MethodOptionsArg
-  >
-}
-
-/**
- * Bound instance methods of a plugin, if defined
- */
-type PluginInstanceMethods<PluginArg extends Plugin> = PluginArg extends {
-  instanceMethods: InstanceMethods
-}
-  ? ErrorInstanceMethods<PluginArg['instanceMethods'], MethodOptions<PluginArg>>
-  : {}
-
-/**
- * Bound instance methods of all plugins
- */
-export type PluginsInstanceMethods<PluginsArg extends Plugins> =
-  UnionToIntersection<PluginInstanceMethods<PluginsArg[number]>> & {}
